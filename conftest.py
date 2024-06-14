3c635328da2fe8a5105f130ec927084deacf62fd
Oksana Gavrush <o6665447@gmail.com>
2024-06-14 20:46:07 +0300

    fixed

--- conftest.py
+++ conftest.py
@@ -4,10 +4,6 @@ import pytest
 from tests_UI_oksana_selenium.pages.create_account_page import CreateNewAccount
 from tests_UI_oksana_selenium.pages.eco_friendly_page import ProductChecker
 from tests_UI_oksana_selenium.pages.sale_page import SaleChecker
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture()
@@ -16,15 +12,9 @@ def driver():
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--disable-gpu')
-    service = Service(ChromeDriverManager().install())
-
-    driver = webdriver.Chrome(service=service, options=options)
+    driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
-    # driver = webdriver.Chrome(options=options)
-    # yield driver
-    # driver.quit()
 
 
 @pytest.fixture()
